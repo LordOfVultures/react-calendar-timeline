7e0f23b98c387b6985960b6acbc5e00865b4c5f0
Yael Villanueva <yael@fireservicerota.com>
2023-07-05 23:56:59 +0100

    Fix groupRenderer not re-rendering

--- src/lib/layout/Sidebar.js
+++ src/lib/layout/Sidebar.js
@@ -1,32 +1,11 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
-import { _get, arraysEqual } from '../utility/generic'
+import { _get } from '../utility/generic'
 
-export default class Sidebar extends Component {
-  static propTypes = {
-    groups: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    groupHeights: PropTypes.array.isRequired,
-    keys: PropTypes.object.isRequired,
-    groupRenderer: PropTypes.func,
-    isRightSidebar: PropTypes.bool,
-  }
-
-  shouldComponentUpdate(nextProps) {
-    return !(
-      nextProps.keys === this.props.keys &&
-      nextProps.width === this.props.width &&
-      nextProps.height === this.props.height &&
-      arraysEqual(nextProps.groups, this.props.groups) &&
-      arraysEqual(nextProps.groupHeights, this.props.groupHeights)
-    )
-  }
-
-  renderGroupContent(group, isRightSidebar, groupTitleKey, groupRightTitleKey) {
-    if (this.props.groupRenderer) {
-      return React.createElement(this.props.groupRenderer, {
+const Sidebar = props => {
+  const renderGroupContent = (group, isRightSidebar, groupTitleKey, groupRightTitleKey) => {
+    if (props.groupRenderer) {
+      return React.createElement(props.groupRenderer, {
         group,
         isRightSidebar
       })
@@ -35,51 +14,51 @@ export default class Sidebar extends Component {
     }
   }
 
-  render() {
-    const { width, groupHeights, height, isRightSidebar } = this.props
+  const { width, groupHeights, height, isRightSidebar } = props
 
-    const { groupIdKey, groupTitleKey, groupRightTitleKey } = this.props.keys
+  const { groupIdKey, groupTitleKey, groupRightTitleKey } = props.keys
 
-    const sidebarStyle = {
-      width: `${width}px`,
-      height: `${height}px`
+  const sidebarStyle = {
+    width: `${width}px`,
+    height: `${height}px`
+  }
+
+  const groupsStyle = {
+    width: `${width}px`
+  }
+
+  let groupLines = props.groups.map((group, index) => {
+    const elementStyle = {
+      height: `${groupHeights[index]}px`,
+      lineHeight: `${groupHeights[index]}px`
     }
-
-    const groupsStyle = {
-      width: `${width}px`
-    }
-
-    let groupLines = this.props.groups.map((group, index) => {
-      const elementStyle = {
-        height: `${groupHeights[index]}px`,
-        lineHeight: `${groupHeights[index]}px`
-      }
-
-      return (
-        <div
-          key={_get(group, groupIdKey)}
-          className={
-            'rct-sidebar-row rct-sidebar-row-' + (index % 2 === 0 ? 'even' : 'odd')
-          }
-          style={elementStyle}
-        >
-          {this.renderGroupContent(
-            group,
-            isRightSidebar,
-            groupTitleKey,
-            groupRightTitleKey
-          )}
-        </div>
-      )
-    })
 
     return (
       <div
-        className={'rct-sidebar' + (isRightSidebar ? ' rct-sidebar-right' : '')}
-        style={sidebarStyle}
+        key={_get(group, groupIdKey)}
+        className={
+          'rct-sidebar-row rct-sidebar-row-' + (index % 2 === 0 ? 'even' : 'odd')
+        }
+        style={elementStyle}
       >
-        <div style={groupsStyle}>{groupLines}</div>
+        {renderGroupContent(
+          group,
+          isRightSidebar,
+          groupTitleKey,
+          groupRightTitleKey
+        )}
       </div>
     )
-  }
+  })
+
+  return (
+    <div
+      className={'rct-sidebar' + (isRightSidebar ? ' rct-sidebar-right' : '')}
+      style={sidebarStyle}
+    >
+      <div style={groupsStyle}>{groupLines}</div>
+    </div>
+  )
 }
+
+export default Sidebar;
